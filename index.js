2bdc670afb666fcbc8ef9542dd6d6ed0eaa09802
Renesha Matthews <reneshamatthews9@gmail.com>
2021-10-13 19:16:03 -0400

    Rename variables with El

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ var countWins = 0
 var countLosses = 0
 
 // Create variables that target elements with the following IDs: 'message', 'wins', 'losses'
-var message = document.getElementById('message')
-var wins = document.getElementById('wins')
-var losses = document.getElementById('losses')
+var messageEl = document.getElementById('message')
+var winsEl = document.getElementById('wins')
+var lossesEl = document.getElementById('losses')
 
 // target all .box elements and attach a click event listener to each one using a loop
 var boxElements = document.querySelectorAll('.box')
@@ -29,18 +29,18 @@ for (i = 0; i < boxElements.length; i++) {
         // if the numbers match, display a winning message by changing the text content of the div#message element
         // if the numbers match, increment wins and display the win count in div#wins
         if (boxNumber === winningBox) {
-            message.textContent = "Hooray! You're a winner!"
+            messageEl.textContent = "Hooray! You're a winner!"
             countWins++
-            wins.textContent = "Wins: " + countWins 
+            winsEl.textContent = "Wins: " + countWins 
         }
 
         // if the numbers don't match, change the div#message element's text to a random losing message from the array above
         // if the numbers don't match, increment losses and display the loss count in div#losses
         if (boxNumber !== winningBox) {
             var randomMessage = losingMessages[Math.floor(Math.random()*losingMessages.length)]
-            message.textContent = randomMessage
+            messageEl.textContent = randomMessage
             countLosses++
-            losses.textContent = "Losses: " + countLosses
+            lossesEl.textContent = "Losses: " + countLosses
         }
     }
     )      
